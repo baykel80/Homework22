8b66f671fb7f16fbbd7661e1c1edf6dafdb62862
Mihael Ištvanović <mistvanov@gmail.com>
2019-05-21 11:34:49 +0200

    Add DOM.searchBtn.addEventListener function

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+"use strict";
+
+const DOM = {
+	resultsContainer: document.querySelector(".js-search_results"),
+	searchBtn: document.querySelector(".js-search_btn")
+}
+
+DOM.searchBtn.addEventListener("click", e => {
+  var number1 = document.getElementById("number1").value;
+  console.log(number1);
+  if(number1==0){
+  var dogApiURL = "https://api.thecatapi.com/v1/images/search?limit=10"+number1;
+  }
+  else{
+    var dogApiURL = "https://api.thecatapi.com/v1/images/search?limit="+number1;
+  }
+  console.log("click");
+	fetchData(dogApiURL)
+		.then(res => {
+    }).catch(function(error) {
+        processError();
+      })
+});
 
